aca0d73bac65d458ed3b503f13ed68088380ce89
RaZegame <75721601+RaZegame@users.noreply.github.com>
2021-09-05 10:25:49 +0300

    Menu pages code

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-const { MessageButton, MessageActionRow } = require('discord.js');
+const { MessageButton, MessageActionRow, Message, MessageSelectMenu } = require('discord.js');
 
-exports.button = async(options = {}) => {
+exports.button = async (options = {}) => {
 
     // Checks
 
     let { interaction, pages, buttons, timeout } = options;
-    if(!interaction || !interaction?.type || interaction?.type !== 'APPLICATION_COMMAND') throw new Error(`INVALID_INTERACTION: There is no valid CommandInteraction provided.`);
-    if(!pages || !(pages instanceof Array) || pages?.length <= 1) throw new Error(`INVALID_PAGES: There is no valid pages Array provided, or the Array's length is 0 / 1.`);
-    if(!timeout || !Number.isInteger(timeout)) timeout = 60000;
+    if (!interaction || !interaction?.type || interaction?.type !== 'APPLICATION_COMMAND') throw new Error(`INVALID_INTERACTION: There is no valid CommandInteraction provided.`);
+    if (!pages || !(pages instanceof Array) || pages?.length <= 1) throw new Error(`INVALID_PAGES: There is no valid pages Array provided, or the Array's length is 0 / 1.`);
+    if (!timeout || !Number.isInteger(timeout)) timeout = 60000;
 
     // Buttons
 
     const previousButton = new MessageButton()
-    .setCustomId('previous')
-    .setLabel(buttons?.previous?.label ? buttons?.previous?.label : 'Previous')
-    .setStyle(buttons?.previous?.style ? buttons?.previous?.style : 'PRIMARY')
-    .setEmoji(buttons?.previous?.emoji ? buttons?.previous?.emoji : '◀');
+        .setCustomId('previous')
+        .setLabel(buttons?.previous?.label ? buttons?.previous?.label : 'Previous')
+        .setStyle(buttons?.previous?.style ? buttons?.previous?.style : 'PRIMARY')
+        .setEmoji(buttons?.previous?.emoji ? buttons?.previous?.emoji : '◀');
 
     const nextButton = new MessageButton()
-    .setCustomId('next')
-    .setLabel(buttons?.next?.label ? buttons?.next?.label : 'Next')
-    .setStyle(buttons?.next?.style ? buttons?.next?.style : 'PRIMARY')
-    .setEmoji(buttons?.next?.emoji ? buttons?.next?.emoji : '▶');
+        .setCustomId('next')
+        .setLabel(buttons?.next?.label ? buttons?.next?.label : 'Next')
+        .setStyle(buttons?.next?.style ? buttons?.next?.style : 'PRIMARY')
+        .setEmoji(buttons?.next?.emoji ? buttons?.next?.emoji : '▶');
 
     const stopButton = new MessageButton()
-    .setCustomId('stop')
-    .setLabel(buttons?.stop?.label ? buttons?.stop?.label : 'Stop')
-    .setStyle(buttons?.stop?.style ? buttons?.stop?.style : 'DANGER')
-    .setEmoji(buttons?.stop?.emoji ? buttons?.stop?.emoji : '⛔');
+        .setCustomId('stop')
+        .setLabel(buttons?.stop?.label ? buttons?.stop?.label : 'Stop')
+        .setStyle(buttons?.stop?.style ? buttons?.stop?.style : 'DANGER')
+        .setEmoji(buttons?.stop?.emoji ? buttons?.stop?.emoji : '⛔');
 
     const row = new MessageActionRow()
-    .addComponents(previousButton, nextButton, stopButton);
+        .addComponents(previousButton, nextButton, stopButton);
 
     // Starting the paginator
 
@@ -50,19 +50,19 @@ exports.button = async(options = {}) => {
     collector.on('collect', async component => {
 
         component.deferUpdate();
-        if(component.user.id !== interaction.user.id) return;
+        if (component.user.id !== interaction.user.id) return;
 
-        if(component.customId === 'previous') {
+        if (component.customId === 'previous') {
 
-            if(currentPage === 0) currentPage = pages.length - 1;
+            if (currentPage === 0) currentPage = pages.length - 1;
             else await currentPage--;
 
             await pages[currentPage].setFooter(`Page ${currentPage + 1}/${pages.length}`);
             interaction.editReply({ embeds: [pages[currentPage]], components: [row] });
 
-        } else if(component.customId === 'next') {
+        } else if (component.customId === 'next') {
 
-            if(currentPage === pages.length - 1) currentPage = 0;
+            if (currentPage === pages.length - 1) currentPage = 0;
             else await currentPage++;
 
             await pages[currentPage].setFooter(`Page ${currentPage + 1}/${pages.length}`);
@@ -84,22 +84,22 @@ exports.button = async(options = {}) => {
 
     collector.on('end', async () => {
 
-        if(stopped === true) return;
+        if (stopped === true) return;
 
         await pages[currentPage].setFooter(`Page ${currentPage + 1}/${pages.length}`);
         await interaction.editReply({ embeds: [pages[currentPage]], components: [row.components[0].setDisabled(true), row.components[1].setDisabled(true), row.components[2].setDisabled(true)] });
-    
+
     });
 };
 
-exports.emoji = async(options = {}) => {
+exports.emoji = async (options = {}) => {
 
     // Checks
 
     let { interaction, pages, emojis, timeout } = options;
-    if(!interaction || !interaction?.type || interaction?.type !== 'APPLICATION_COMMAND') throw new Error(`INVALID_INTERACTION: There is no valid CommandInteraction provided.`);
-    if(!pages || !(pages instanceof Array) || pages?.length <= 1) throw new Error(`INVALID_PAGES: There is no valid pages Array provided, or the Array's length is 0 / 1.`);
-    if(!timeout || !Number.isInteger(timeout)) timeout = 60000;
+    if (!interaction || !interaction?.type || interaction?.type !== 'APPLICATION_COMMAND') throw new Error(`INVALID_INTERACTION: There is no valid CommandInteraction provided.`);
+    if (!pages || !(pages instanceof Array) || pages?.length <= 1) throw new Error(`INVALID_PAGES: There is no valid pages Array provided, or the Array's length is 0 / 1.`);
+    if (!timeout || !Number.isInteger(timeout)) timeout = 60000;
 
     // Starting the paginator
 
@@ -122,19 +122,19 @@ exports.emoji = async(options = {}) => {
     collector.on('collect', async (reaction, user) => {
 
         await reaction.message.resolve(reaction.emoji.id).users.remove(user.id);
-        if(user.id !== interaction.user.id) return;
-        
-        if(reaction.emoji.name === emojis?.previous ? emojis?.previous : '◀') {
+        if (user.id !== interaction.user.id) return;
 
-            if(currentPage === 0) currentPage = pages.length - 1;
+        if (reaction.emoji.name === emojis?.previous ? emojis?.previous : '◀') {
+
+            if (currentPage === 0) currentPage = pages.length - 1;
             else await currentPage--;
 
             await pages[currentPage].setFooter(`Page ${currentPage + 1}/${pages.length}`);
             interaction.editReply({ embeds: [pages[currentPage]] });
 
-        } else if(reaction.emoji.name === emojis?.next ? emojis?.next : '▶') {
+        } else if (reaction.emoji.name === emojis?.next ? emojis?.next : '▶') {
 
-            if(currentPage === pages.length - 1) currentPage = 0;
+            if (currentPage === pages.length - 1) currentPage = 0;
             else await currentPage++;
 
             await pages[currentPage].setFooter(`Page ${currentPage + 1}/${pages.length}`);
@@ -149,5 +149,38 @@ exports.emoji = async(options = {}) => {
 
         };
 
+    });
+};
+
+exports.menuPages = async (/** @type {Message} */ message, pages, timeout, menuOptions) => {
+
+    const Menu = new MessageSelectMenu()
+        .setCustomId("sel_menu_pages")
+        .setPlaceholder((menuOptions?.placeHolder) ? menuOptions.placeHolder : "Select something!")
+        .addOptions(pages);
+
+    const msg = await message.channel.send({ embeds: [pages[0].embed], components: [new MessageActionRow().addComponents(Menu)] }).catch(() => {});
+    if(!msg) {
+        throw new Error("Sent message was deleted or not sent. No possibility to collect.");
+    }
+    const col = message.channel.createMessageComponentCollector({ componentType: "SELECT_MENU", filter: (int) => int.user.id === message.author.id, dispose: true, time: timeout, idle: timeout / 2 });
+    col.on("collect", async (int) => {
+        if (int.isSelectMenu()) {
+            for await (const value of int.values) {
+                for (const page of pages) {
+                    if (page.value === value) {
+                        await int.deferUpdate().catch(() => { });
+                        await msg.edit({ embeds: [page.embed] });
+                        return;
+                    } else {
+                        continue;
+                    }
+                }
+            }
+        }
+    });
+    col.once("end", async () => {
+        const disabledActionrow = new MessageActionRow().addComponents(Menu.setDisabled(true));
+        await msg.edit({ embeds: [pages[0].embed], components: [disabledActionrow] }).catch(() => { });
     });
 };
